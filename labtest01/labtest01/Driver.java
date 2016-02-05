c3c9ac254859a44f38dc22a086a95a7aa260ffec
dkanda <dkanda@lab2-41.CS.McGill.CA>
2016-02-05 09:36:45 -0500

    Rough changes

--- labtest01/labtest01/Driver.java
+++ labtest01/labtest01/Driver.java
@@ -1,5 +1,8 @@
 package labtest01;
 
+import java.awt.List;
+import java.util.Collections;
+
 /**
  * Utility class with a driver program and some 
  * sample items and inventories.
@@ -19,9 +22,22 @@ public final class Driver
 	 */
 	public static void main(String[] pArgs)
 	{
+		Inventory inven = new Inventory("Test");
 		for( Item item : ITEMS )
 		{
 			System.out.println(item.getName());
+			inven.stock(item, 1);
+			
 		}
+		
+		/** Test for totalValue() **/
+		System.out.println("Total Value is: " +inven.totalValue()); 
+		
+		/** Test for assert **/
+		inven.dispose(ITEM_CEREAL, 2);
+		
+		List test = new List();
+		/** Test for Comparator **/
+		//Collections.sort(List<Item>);
 	}
 }
